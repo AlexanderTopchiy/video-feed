5989ec6ed4ab2ae7168f9b36c4eae987b4de6ac7
Alexander Topchiy <topchiyau@gmail.com>
2019-11-19 23:09:18 +0300

    Add display single video to the list screen

--- lib/video_list_screen.dart
+++ lib/video_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_feed/video_widget.dart';
 
 class VideoListScreen extends StatefulWidget {
   @override
@@ -9,6 +10,19 @@ class _VideoListScreenState extends State<VideoListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          height: 300.0,
+          alignment: Alignment.center,
+          margin: EdgeInsets.symmetric(vertical: 50.0),
+          child: VideoWidget(
+              isPlaying: true,
+              urlSource: 'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+          ),
+        ),
+      ),
+    );
   }
 }
